775a488487fe2fd293c725eeb73b936db96aaa26
Indecastle <andred9991@gmail.com>
2020-01-30 16:15:57 +0300

    added page 'CreateCompany'

---
+++ ViewModels/CompanyViewModel.cs
@@ -0,0 +1,46 @@
+﻿using BlazorInputFile;
+using Crowfounding.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crowfounding.ViewModels
+{
+    public class CompanyViewModel
+    {
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Theme")]
+        public Theme Theme { get; set; }
+
+        [Required]
+        [Display(Name = "Data End")]
+        [DataType(DataType.Date)]
+        public DateTime End { get; set; }
+
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        [Required]
+        [Display(Name = "Need Money")]
+        [Range(10, 100000, ErrorMessage = "Accommodation invalid (10-100000).")]
+        public int NeedMoney { get; set; }
+
+        [Required]
+        [Display(Name = "URL")]
+        public string URLVideo { get; set; }
+
+        [Required(AllowEmptyStrings = false, 
+            ErrorMessage = "need Main Image")]
+        public IFileListEntry MainImage { get; set; }
+
+        [Display(Name = "Images")]
+        public IFileListEntry[] Images { get; set; }
+    }
+}
