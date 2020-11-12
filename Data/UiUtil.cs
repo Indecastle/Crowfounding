91be8db0be128fc07a9c83c4d511326fa8e02c78
markiz <andred9991@gmail.com>
2020-11-13 01:13:25 +0300

    Updated UI Settings page, other updated UI

---
+++ Data/UiUtil.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace Crowfounding.Data
+{
+    public class UiUtil
+    {
+        public const string defaultFormatDecimal = "0.00";
+        public static readonly IFormatProvider cultureUS = new CultureInfo("en-US");
+
+        public static string FormatDecimal(decimal value, string format = defaultFormatDecimal)
+        {
+            return value.ToString(format, cultureUS);
+        }
+    }
+}
